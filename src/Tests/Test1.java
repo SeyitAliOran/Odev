d28e7f764a86e283b99df84f4efa061a52b0c4b6
Seyit <seyitaligermany@gmail.com>
2023-09-19 14:12:24 +0200

    Test1 name changed

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -1,6 +1,6 @@
 package Tests;
 
-public class Test11 {
+public class Test1 {
     public static void main(String[] args) {
         System.out.println("I am Test1");
          System.out.println("I am at Remoote updated");
