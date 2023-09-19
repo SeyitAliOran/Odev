5a12d6c94cd9297493a2ebdfad7131299d769940
Seyit <seyitaligermany@gmail.com>
2023-09-19 14:14:17 +0200

    Branch updated

--- src/Tests/Test2.java
+++ src/Tests/Test2.java
@@ -3,6 +3,7 @@ package Tests;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("I am Test2");
+        System.out.println("I am at BH11-1001_Login_Test branch");
     }
 }
 
